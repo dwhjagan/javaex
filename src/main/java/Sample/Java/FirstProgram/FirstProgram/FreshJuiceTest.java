9197dca4ba850da71948f9aca50a01ec32cdfdbe
Cyndi Rader <janaidu02@gmial.com>
2016-12-17 16:36:15 +0000

    5thd phase commit point

--- src/main/java/Sample/Java/FirstProgram/FirstProgram/FreshJuiceTest.java
+++ src/main/java/Sample/Java/FirstProgram/FirstProgram/FreshJuiceTest.java
@@ -1,15 +1,17 @@
 package Sample.Java.FirstProgram.FirstProgram;
 
 class FreshJuice {
-  enum FreshJuiceSize{ SMALL, MEDIUM, LARGE }
-  FreshJuiceSize size;
+
+  enum FreshJuiceSize{ SMALL, MEDIUM1, LARGE }      // enum
+  FreshJuiceSize size123;
 }
 
 public class FreshJuiceTest {
 
   public static void main(String args[]) {
+
     FreshJuice juice = new FreshJuice();
-    juice.size = FreshJuice.FreshJuiceSize.MEDIUM ;
-    System.out.println("Size: " + juice.size);
+    juice.size123 = FreshJuice.FreshJuiceSize.MEDIUM1 ;
+    System.out.println("Size: " + juice.size123);
   }
 }
