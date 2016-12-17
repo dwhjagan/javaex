4d18b4aba2b05a36a925193f2f01ea0310c8ddc9
Cyndi Rader <janaidu02@gmial.com>
2016-12-17 18:16:11 +0000

    6thd phase commit point

---
+++ src/main/java/Sample/Java/FirstProgram/variable/v2.java
@@ -0,0 +1,13 @@
+package Sample.Java.FirstProgram.variable;
+
+public class v2{
+
+    public static void main(String []args){
+
+        int a = 10;
+        String b = "Jagan";
+        System.out.println(a+"  "+ b);
+
+    }
+
+}
---
+++ src/main/java/Sample/Java/FirstProgram/variable/var.java
@@ -0,0 +1,32 @@
+package Sample.Java.FirstProgram.variable;
+import java.util.Map;
+
+public class var {
+    public static void main(String []args)
+
+    {
+
+        int a = 42;
+        int b = 50;
+        int c = 45;
+
+        String one = "1";
+        String two = "2";
+        String three = "3";
+        byte bc = 22;
+        double pi = 3.14159;
+
+        // int a, b, c;         // Declares three ints, a, b, and c.
+       // int a = 10, b = 10;  // Example of initialization
+      //  byte B = 22;         // initializes a byte type variable B.
+      //  double pi = 3.14159; // declares and assigns a value of PI.
+       // char a = 'a';        // the char variable a iis initialized with value 'a'
+
+        System.out.println(a+b+c);
+        System.out.println("one: " + one + " and two: " + two +
+                           " and also a three: " + three);
+        System.out.println(pi+ "  "+ bc);
+
+
+    }
+}
