c3d3ec1fb2d9dd7ec871783bbb62577d4ce49f67
Cyndi Rader <janaidu02@gmial.com>
2016-12-19 23:50:59 +0000

    2nd commit

---
+++ src/main/java/Edu2/test.java
@@ -0,0 +1,15 @@
+package Edu2;
+
+/**
+ * Created by janaidu on 19/12/2016.
+ */
+
+
+public class test {
+    public static void main(String[] args) {
+
+            System.err.println("Usage: MonthFromNumber <month>"+ args.length);
+
+
+        }
+    }
